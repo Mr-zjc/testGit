febfc9b93d0c4f47891693c5b547143faf8868f1
Mr-zjc <15571047855@163.com>
2022-05-29 17:24:41 +0800

    first commit

---
+++ src/com/testGit.java
@@ -0,0 +1,13 @@
+package com;
+
+import sun.applet.Main;
+
+/**
+ * @author ZJC
+ * @create 2022-05-29 17:19
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
